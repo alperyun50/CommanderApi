609b7a221cc4449604df759bc5e2a4ff9f7bd6bd
alper yavuz ün <yavuzun50@hotmail.com>
2022-03-02 19:58:38 +0300

    EF Packages added

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommanderApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace CommanderApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            // added for service lifetimes
+            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
